694fb634c59ec494caa34f6d58e171d3d80fb85f
Antonio Narino <anarino@students.avc.edu>
2024-02-26 00:35:24 -0800

    first function - rewrite split words - reading/creating file - WORKING

--- main.hpp
+++ main.hpp
@@ -1,10 +1,77 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 int rewritesplitwords(string, char);
 string MFN(string stname, int thisyear);
 
+struct Name {
+    string stname;
+    string gender;
+    int year;
+    string bname;
+    int count;
+} ;
+
+int rewritesplitwords(string x, char delim) {
+    ifstream ifs;
+    ofstream ofs;
+    string line, buff;
+    stringstream ss;
+    Name name;
+    int fcnt;
+    int cntr = 0;
+
+    ifs.open(x);
+    if(!ifs) {
+        cerr << "FILE COOKED" << endl;
+        exit(0);
+    }
+
+    ofs.open("split.txt");
+    if(!ofs) {
+        cerr << "FILE COOKED" << endl;
+        exit(0);
+    }
+
+    while(getline(ifs, line)) {
+        ss << line;
+        fcnt = 0;
+        while(getline(ss, buff, delim)) {
+            switch(fcnt) {
+                case 0:
+                    name.stname = buff;
+                    break;
+                case 1:
+                    name.gender = buff;
+                    break;
+                case 2:
+                    name.year = stoi(buff);
+                    break;
+                case 3:
+                    name.bname = buff;
+                    break;
+                case 4:
+                    name.count = stoi(buff);
+                    break;
+                default:
+                    cerr << "FCNT ERROR" << endl;
+                    break;
+            }
+            fcnt++;
+        }
+        ofs << name.stname << "\t" << name.gender << "\t" << name.year << "\t" << name.bname << "\t" << name.count << endl;
+        ss.clear();
+        cntr++;
+    }
+
+    ifs.close();
+    ofs.close();
+
+    return cntr;
+}
 /*******************************
  * Code your program here
  *******************************/
